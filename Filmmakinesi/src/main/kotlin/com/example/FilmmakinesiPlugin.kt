14e0b3088625a27523a5f88acb9dbf7b64618ac8
NematMaharramov <meherremovnemat81@gmail.com>
2026-01-11 19:11:41 +0400

    Add ExamplePlugin class for FilmMakinesi
    Implement ExamplePlugin for FilmMakinesi with settings.

---
+++ Filmmakinesi/src/main/kotlin/com/example/FilmmakinesiPlugin.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class ExamplePlugin: Plugin() {
+    private var activity: AppCompatActivity? = null
+
+    override fun load(context: Context) {
+        activity = context as? AppCompatActivity
+
+        // FilmMakinesiProvider'ı kaydediyoruz
+        registerMainAPI(FilmMakinesiProvider())
+
+        openSettings = {
+            val frag = BlankFragment(this)
+            activity?.let {
+                frag.show(it.supportFragmentManager, "Frag")
+            }
+        }
+    }
+}
